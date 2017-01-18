80368e8a068611e15de42867efc998fcacf81201
Perry Berman <R3alCl0ud@users.noreply.github.com>
2017-01-18 11:51:55 -0700

    Create EventHandler.java
    More for event stuff

---
+++ src/util/EventHandler.java
@@ -0,0 +1,14 @@
+package util;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Documented
+@Target(ElementType.METHOD)
+public @interface EventHandler {
+  
+}
