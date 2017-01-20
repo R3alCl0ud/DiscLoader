3e360d923d1c722d51c10f72dfb42dc9ee1666c1
Zachary <r3alcl0ud@gmail.com>
2017-01-20 13:56:50 -0700

    created documentation

--- src/main/java/com/forgecord/structures/Guild.java
+++ src/main/java/com/forgecord/structures/Guild.java
@@ -24,17 +24,54 @@ public class Guild {
 	public int mfaLevel;
 	public int memberCount;
 	
+	/**
+	 * 
+	 */
 	public boolean embedEnabled;
+	
+	/**
+	 * iss the guild large (more than 250 members)
+	 */
 	public boolean large;
+	
+	/**
+	 * is the guild unavailable
+	 */
 	public boolean unavailable;
 	
+	/**
+	 * 
+	 */
 	public Date joinedAt;
 	
+	/**
+	 * The guild's channels stored as {@link HashMap}<{@link String Snowflake}, {@link GuildChannel}>
+	 * @author perryberman
+	 */
 	public HashMap<String, GuildChannel> channels;
+	
+	/**
+	 * The guild's members stored as {@link HashMap}<{@link String Snowflake}, {@link GuildMember}>
+	 * @author Perry Berman
+	 */
 	public HashMap<String, GuildMember> members;
+	
+	/**
+	 * The guild's roles stored as {@link HashMap}<{@link String Snowflake}, {@link Role}>
+	 * @author perryberman
+	 */
 	public HashMap<String, Role> roles;
+	
+	/**
+	 * The guild's emojis stored as {@link HashMap}<{@link String Snowflake}, {@link Emoji}>
+	 * @author perryberman
+	 */
 	public HashMap<String, Emoji> emojis;
 	
+	/**
+	 * @param client
+	 * @param data
+	 */
 	public Guild(Client client, JSONObject data) {
 		this.client = client;
 		this.id = data.getString("id");
@@ -56,8 +93,12 @@ public class Guild {
 		
 	}
 
+	/**
+	 * @param guildMember
+	 * @param emitEvent 
+	 * @return {@link GuildMember GuildMember}
+	 */
 	public GuildMember addMember(Object guildMember, boolean emitEvent) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 }
