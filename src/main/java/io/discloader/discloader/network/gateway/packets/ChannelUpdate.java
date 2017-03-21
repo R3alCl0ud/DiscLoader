bdcd35a3b0f6f748f1e1232e6806b0808dfbdf50
Perry Berman <R3alCl0ud@users.noreply.github.com>
2017-03-21 11:21:50 -0700

    save old channel instance
    added logic to preserve a channel's previous state when receiving a `CHANNEL_UPDATE` packet

--- src/main/java/io/discloader/discloader/network/gateway/packets/ChannelUpdate.java
+++ src/main/java/io/discloader/discloader/network/gateway/packets/ChannelUpdate.java
@@ -10,7 +10,7 @@ import io.discloader.discloader.entity.channels.Channel;
 import io.discloader.discloader.entity.guild.Guild;
 import io.discloader.discloader.network.gateway.DiscSocket;
 import io.discloader.discloader.network.json.ChannelJSON;
-import io.discloader.discloader.util.DLUtil;
+import io.discloader.discloader.util.DLUtil.Events;
 
 /**
  * @author Perry Berman
@@ -23,18 +23,19 @@ public class ChannelUpdate extends DLPacket {
 	}
 	
 	public void handle(SocketPacket packet) {
-		String d = this.gson.toJson(packet.d);
-		ChannelJSON data = this.gson.fromJson(d, ChannelJSON.class);
+		String d = gson.toJson(packet.d);
+		ChannelJSON data = gson.fromJson(d, ChannelJSON.class);
 		Guild guild = null;
+		Channel oldChannel = loader.channels.get(data.id);
 		Channel channel = null;
 		if (data.guild_id != null) {
-			guild = this.socket.loader.guilds.get(data.guild_id);
-			channel = this.socket.loader.addChannel(data, guild);
+			guild = loader.guilds.get(data.guild_id);
+			channel = loader.addChannel(data, guild);
 		} else {
-			channel = this.socket.loader.addChannel(data);
+			channel = loader.addChannel(data);
 		}
-		ChannelUpdateEvent event = new ChannelUpdateEvent(channel);
-		this.socket.loader.emit(DLUtil.Events.CHANNEL_UPDATE, event);
+		ChannelUpdateEvent event = new ChannelUpdateEvent(channel, oldChannel);
+		loader.emit(Events.CHANNEL_UPDATE, event);
 		for (IEventListener e : DiscLoader.handlers.values()) {
 			e.ChannelUpdate(event);
 		}
