b79b929a96b26775caa69dc7f0f94bc3dc893639
Perry Berman <R3alCl0ud@users.noreply.github.com>
2017-03-21 11:23:23 -0700

    update ChannelUpdateEvent
    ChannelUpdateEvent has been updated to include the channel's state

--- src/main/java/io/discloader/discloader/common/event/ChannelUpdateEvent.java
+++ src/main/java/io/discloader/discloader/common/event/ChannelUpdateEvent.java
@@ -6,17 +6,27 @@ package io.discloader.discloader.common.event;
 import io.discloader.discloader.entity.channels.Channel;
 
 /**
+ * 
+ * 
  * @author Perry Berman
- *
  */
 public class ChannelUpdateEvent extends DLEvent {
 
-	public final Channel channel;
-
-	public ChannelUpdateEvent(Channel channel) {
+	private final Channel channel;
+	private final Channel oldChannel;
+	public ChannelUpdateEvent(Channel channel, Channel oldChannel) {
 		super(channel.loader);
 		this.channel = channel;
+		this.oldChannel = oldChannel;
 
+	}
+	
+	public Channel getChannel() {
+		return channel;
+	}
+	
+	public Channel getOldChannel() {
+		return oldChannel;
 	}
 
 }
